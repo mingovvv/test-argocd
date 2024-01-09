3e7a4d602c546bf5a1f06c164af917f364a0df26
mk.jang <mk.jang@twolinecode.com>
2024-01-09 15:51:36 +0900

    Feat : "agrocd 테스트"

---
+++ src/test/java/test/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package test.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
