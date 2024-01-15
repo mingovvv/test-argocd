3f04ee7214e6499546572f85030582227900e251
mk.jang <mk.jang@twolinecode.com>
2024-01-15 14:58:41 +0900

    Feat : "HPA 오브젝트 추가, Deployment 오브젝트 수정 : v2.4"

--- src/main/java/test/web/HomeController.java
+++ src/main/java/test/web/HomeController.java
@@ -1,9 +1,12 @@
 package test.web;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -18,4 +21,13 @@ public class HomeController {
         model.addAttribute("profile", activeProfile);
         return "index";
     }
+
+    @ResponseBody
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity
+                .status(Response.SC_OK)
+                .body("this is a health check");
+    }
+
 }
